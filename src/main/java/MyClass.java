ba6287b87d6eeb3174e48d756192445e8fd9e26d
innazhurba <inazhurba@gmail.com>
2021-11-29 14:21:39 +0200

    new initial commit

---
+++ src/main/java/MyClass.java
@@ -0,0 +1,6 @@
+public class MyClass {
+    private int arr[];//передається за посиланням
+    public MyClass(int arr[]){
+        this.arr = arr;
+    }
+}
--- src/main/java/ua/edu/ucu/tempseries/TemperatureSeriesAnalysis.java
+++ src/main/java/ua/edu/ucu/tempseries/TemperatureSeriesAnalysis.java
@@ -1,13 +1,33 @@
 package ua.edu.ucu.tempseries;
 
+import lombok.Getter;
+
+import java.util.Arrays;
+import java.util.InputMismatchException;
+
+@Getter
 public class TemperatureSeriesAnalysis {
+    private double[] temperatureSeries;
+    private int length;
 
     public TemperatureSeriesAnalysis() {
-
+        temperatureSeries = new double[]{};
+        length = temperatureSeries.length;
     }
 
     public TemperatureSeriesAnalysis(double[] temperatureSeries) {
-
+        if(temperatureSeries == null || temperatureSeries.length==0)
+            return;
+        checkSeriesIsValid(temperatureSeries);
+        this.length = temperatureSeries.length;
+        this.temperatureSeries = Arrays.copyOf(temperatureSeries, temperatureSeries.length);
+    }
+    public void checkSeriesIsValid(double[] temperatureSeries){
+        int minTemp = -273;
+        for(int i=0;i<temperatureSeries.length;i++){
+            if(temperatureSeries[i]<=minTemp)
+                throw new InputMismatchException();
+        }
     }
 
     public double average() {
@@ -27,6 +47,19 @@ public class TemperatureSeriesAnalysis {
     }
 
     public double findTempClosestToZero() {
+        if(getTemperatureSeries().length==0)
+            throw new IllegalArgumentException();
+
+        double minDiff = Double.POSITIVE_INFINITY;
+        double closestValue;
+        for(double temp: temperatureSeries){
+            if(Math.abs(temp-0) < minDiff){
+                minDiff = temp;
+            }
+            if(temp>0 && Math.abs(temp-0)==minDiff){
+
+            }
+        }
         return 0;
     }
 

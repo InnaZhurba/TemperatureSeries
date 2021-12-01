49656a6f2635b2179378ce8fea53343c1f453593
innazhurba <inazhurba@gmail.com>
2021-12-01 11:21:08 +0200

    edit local issues with whitespaces in statistics

--- src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
+++ src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
@@ -6,7 +6,10 @@ import lombok.Getter;
 public class TempSummaryStatistics {
     private double averageTemp, deviationTemp, minTemp, maxTemp;
 
-    public TempSummaryStatistics(double averageTemp, double deviationTemp, double minTemp, double maxTemp) {
+    public TempSummaryStatistics(double averageTemp,
+                                 double deviationTemp,
+                                 double minTemp,
+                                 double maxTemp) {
         this.averageTemp = averageTemp;
         this.deviationTemp = deviationTemp;
         this.minTemp = minTemp;
